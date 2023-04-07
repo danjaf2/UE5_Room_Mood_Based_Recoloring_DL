d95b37d6fc71fba1e5e912c1e8ae93d44cc6f1d0
nightnimbus <nightnimbus@gmail.com>
2023-04-07 02:16:33 -0400

    add base MyOptimizer class skeleton

---
+++ COMP_475_Project/Source/COMP_475_Project/MyOptimizer.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyOptimizer.h"
+
+MyOptimizer::MyOptimizer()
+{
+}
+
+void MyOptimizer::OptimizeSceneForMoods()
+{
+	UE_LOG(LogTemp, Warning, TEXT("in OptimizeSceneForMoods()"));
+}
+
+MyOptimizer::~MyOptimizer()
+{
+}
---
+++ COMP_475_Project/Source/COMP_475_Project/MyOptimizer.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COMP_475_PROJECT_API MyOptimizer
+{
+public:
+	MyOptimizer();
+	void OptimizeSceneForMoods();
+	~MyOptimizer();
+};
